29d8fe8dca906d92dde61f08270214e564856ab0
nglaeser <nglaeser@users.noreply.github.com>
2019-03-05 11:39:54 -0500

    add graph-cyclone (new name) to pypi

--- graph-cyclone/setup.py
+++ graph-cyclone/setup.py
@@ -4,14 +4,14 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='graphcyclone',
+   name='graph-cyclone',
    version='1.0',
    description='Utilities for powers of cycle graphs',
    license="GPL",
    long_description=long_description,
    author='Noemi Glaeser',
    author_email='',
-   url="https://github.com/nglaeser/graphcyclone",
-   packages=['graphcyclone'],  #same as name
+   url="https://github.com/nglaeser/graph-cyclone",
+   packages=['graph-cyclone'],  #same as name
    install_requires=['numpy', 'random'], #external packages as dependencies
 )
